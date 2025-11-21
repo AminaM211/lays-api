e3d0115e6ee9e138f1dfdf53a731c5225c85f178
AminaM211 <148769375+AminaM211@users.noreply.github.com>
2025-11-21 12:55:00 +0100

    Add MongoDB connection

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import bagRoutes from "./routes/bagRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
+import mongoose from "mongoose";
 
 dotenv.config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// routes
+import bagRoutes from "./routes/bagRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 app.use("/api/v1/bag", bagRoutes);
 app.use("/api/v1/user", userRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("API running on port " + process.env.PORT);
-});
+// connect database + start server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database connected");
+    app.listen(process.env.PORT, () => {
+      console.log("API running on port " + process.env.PORT);
+    });
+  })
+  .catch((err) => console.log("DB error:", err));
