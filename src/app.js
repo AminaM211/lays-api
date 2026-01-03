173a1e4c3081531d640f5ead347694548887d0a4
AminaM211 <148769375+AminaM211@users.noreply.github.com>
2026-01-03 13:48:57 +0100

    fix: refactor CORS configuration to use options for pre-flight requests

--- src/app.js
+++ src/app.js
@@ -4,22 +4,25 @@ import cookieParser from "cookie-parser"
 
 import bagRoutes from "./routes/bagRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
-// import voteRoutes from "./routes/voteRoutes.js"
 
 const app = express()
 
 app.use(cookieParser())
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://lays-vue-2.vercel.app",
-      "https://lays-configurator-vert.vercel.app"
-    ],
-    credentials: true
-  })
-)
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://lays-vue-2.vercel.app",
+    "https://lays-configurator-vert.vercel.app"
+  ],
+  credentials: true
+}
+
+// 🔑 PRE-FLIGHT (OPTIONS)
+app.options("*", cors(corsOptions))
+
+// 🔑 ECHTE REQUESTS
+app.use(cors(corsOptions))
 
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true }))
